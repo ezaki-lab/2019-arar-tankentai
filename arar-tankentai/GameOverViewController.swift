e821b1cdf131bc0b9ec00498dc0f5a313a2484f7
Kaito Hattori <kaichan394@gmail.com>
2019-10-25 15:22:40 +0900

    add back home key button

--- arar-tankentai/GameOverViewController.swift
+++ arar-tankentai/GameOverViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class GameOverViewController: UIViewController {
     
     @IBOutlet weak var scoreView: UIView!
     @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var backHomeKeyButton1: UIButton!
+    @IBOutlet weak var backHomeKeyButton2: UIButton!
+    @IBOutlet weak var backHomeKeyButton3: UIButton!
+    @IBOutlet weak var backHomeKeyButton4: UIButton!
+    var backHomeKey: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,45 @@ class GameOverViewController: UIViewController {
         self.scoreView.layer.cornerRadius = self.scoreView.frame.size.width / 2
         
         self.scoreLabel.text = String(scoreManager.score)
+    }
+    
+    @IBAction func clickedBackHomeKeyButton1(_ sender: Any) {
+        if self.backHomeKey == 0 {
+            self.backHomeKey = 1
+        }
+    }
+    
+    @IBAction func clickedBackHomeKeyButton2(_ sender: Any) {
+        if self.backHomeKey == 1 {
+            self.backHomeKey = 2
+        }
+    }
+    
+    @IBAction func clickedBackHomeKeyButton3(_ sender: Any) {
+        if self.backHomeKey == 2 {
+            self.backHomeKey = 3
+        }
+    }
+    
+    @IBAction func clickedBackHomeKeyButton4(_ sender: Any) {
+        if self.backHomeKey == 3 {
+            self.backHomeKey = 0
+            
+            let alert = SCLAlertView()
+            let txt = alert.addTextField("Enter password")
+            alert.addButton("Done") {
+                if txt.text == "hattori" || txt.text == "Hattori" {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+                    }
+                }
+                else {
+                    let eAlert = SCLAlertView()
+                    eAlert.showError("エラー", subTitle: "パスワードが間違っています。")
+                }
+            }
+            alert.showEdit("運営アラート", subTitle: "パスワードを入力してください。\nユーザーは「キャンセル」を押してください。", closeButtonTitle: "キャンセル")
+        }
     }
     
 }
